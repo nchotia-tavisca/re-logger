79c9f4265a59cc155ab218e9cd1050bb47c7c329
Suraj Acharya <suraj.acharya@coditas.com>
2020-01-16 15:26:27 +0530

    Fixes

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
-import { ActionReducer } from '@ngrx/store';
 
-export const customMiddleWare = (reducer: ActionReducer<any>): ActionReducer<any> => (state, action) => {
+export const customMiddleWare = (reducer) => (state, action) => {
     console.log('action',action);
     console.log('state',state);
     return reducer(state, action)
