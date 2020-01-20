0af3e952a21388bf03497241d0cc2a8ff745e5d7
Suraj Acharya <suraj.acharya@coditas.com>
2020-01-20 15:38:40 +0530

    Changes

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,17 @@
 import { ActionReducer } from "@ngrx/store";
+export class Middleware {
+  private constructor() {}
+  static getInstance() {
+    if (!(window as any).middleware) {
+      (window as any).middleware = new Middleware();
+    }
+    return (window as any).middleware;
+  }
 
-export function logState(reducer: ActionReducer<any>): ActionReducer<any> {
-  return function(state, action) {
-    console.log(action);
-    return reducer(state, action);
-  };
-};
+  logState(reducer: ActionReducer<any>): ActionReducer<any> {
+    return function(state, action) {
+      console.log(action);
+      return reducer(state, action);
+    };
+  }
+}
